5c815b5f65c8cd17382bec51178b0b4c0fb80268
Kevin Hunter <vudugrv@hotmail.com>
2016-12-02 17:54:08 -0800

    Added adaptive music

---
+++ Assets/_Scripts/Event_Slider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using FMOD;
+using FMODUnity;
+
+public class Event_Slider : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string eventName;
+
+    FMOD.Studio.EventInstance eventInst;
+    //FMOD.Studio.ParameterInstance paramInst;
+
+    public Slider slider;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        eventInst = FMODUnity.RuntimeManager.CreateInstance(eventName);
+        //paramInst = FMODUnity.RuntimeManager.CreateInstance(parameterName);
+    }
+
+    void Update()
+    {
+        eventInst.setParameterValue("Intensity", slider.value);
+    }
+}
---
+++ Assets/_Scripts/Play_Button.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using FMOD;
+using FMODUnity;
+
+public class Play_Button : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string eventName;
+
+    FMOD.Studio.EventInstance eventInst;
+
+    public Button button;
+
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(SoundOnClick);
+
+        eventInst = FMODUnity.RuntimeManager.CreateInstance(eventName);
+    }
+
+    void SoundOnClick()
+    {
+        eventInst.start();
+    }
+}
